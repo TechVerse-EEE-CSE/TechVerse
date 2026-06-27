085cdf9ce5a0fa1d9d33ba4aa83e0a7135961671
IMRAN AHMED <imran.info.me@gmail.com>
2026-06-27 10:19:33 +0600

    Create firebase-config.js

---
+++ tech-verse-code-editor/firebase-config.js
@@ -0,0 +1,16 @@
+// ══════════════════════════════════════
+//  FIREBASE CONFIG — এই ফাইলটি .gitignore এ রাখুন!
+//  GitHub বা পাবলিক জায়গায় আপলোড করবেন না।
+// ══════════════════════════════════════
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDRfX8cqKmDRV2u1HfH2opa5tB2NqD0h9s",
+  authDomain: "techverse-eee-cse.firebaseapp.com",
+  projectId: "techverse-eee-cse",
+  storageBucket: "techverse-eee-cse.firebasestorage.app",
+  messagingSenderId: "719941320241",
+  appId: "1:719941320241:web:10285cc72447a811ad5f10",
+  measurementId: "G-Y1VFHYHH26"
+};
+
+export default firebaseConfig;
